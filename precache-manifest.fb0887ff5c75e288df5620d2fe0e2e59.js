a6756eed314aff4e954888227d49f8b46b57de94
OleynikDima <57680550+OleynikDima@users.noreply.github.com>
2020-07-25 12:48:44 +0300

    Updates

--- precache-manifest.fb0887ff5c75e288df5620d2fe0e2e59.js
+++ precache-manifest.fb0887ff5c75e288df5620d2fe0e2e59.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5272853ee7a51d9f68a6bd25f146e934",
+    "revision": "7d664a62478790b971156c0089fc82c4",
     "url": "/goit-react-hw-01-components/index.html"
   },
   {
-    "revision": "6fa401e45c4107761867",
-    "url": "/goit-react-hw-01-components/static/css/main.92a05aac.chunk.css"
+    "revision": "0053f97adc249ba697bc",
+    "url": "/goit-react-hw-01-components/static/css/main.e768b014.chunk.css"
   },
   {
     "revision": "0b6fca7455a7e55f600d",
@@ -16,11 +16,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/goit-react-hw-01-components/static/js/2.e8cad29a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6fa401e45c4107761867",
-    "url": "/goit-react-hw-01-components/static/js/main.1b221010.chunk.js"
+    "revision": "0053f97adc249ba697bc",
+    "url": "/goit-react-hw-01-components/static/js/main.c8be80bb.chunk.js"
   },
   {
     "revision": "0792ce1863c9ad6eb546",
     "url": "/goit-react-hw-01-components/static/js/runtime-main.e512bb13.js"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/goit-react-hw-01-components/static/media/Lato-Regular.2d36b1a9.ttf"
   }
 ]);
